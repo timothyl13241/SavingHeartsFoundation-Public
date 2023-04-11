7d62997872412638df994ed3444e25dbf6f0a048
Timothy Le <14115751+timothyle97@users.noreply.github.com>
2023-04-11 14:20:13 -0700

    Update map_drives.bat
    Add net use to end of script to show mapped drives. 
    End termination behavior.

--- map_drives.bat
+++ map_drives.bat
@@ -10,3 +10,7 @@ cmdkey /add:172.30.16.234 /user:SHF\SHF_USER /pass:%passw%
 net use * /delete /y
 net use S: \\172.30.16.234\UCLA_SHF_Data %passw% /user:SHF\SHF_USER /p:yes
 net use U: \\172.30.16.234\UCLA_Deployment %passw% /user:SHF\SHF_USER /p:yes
+pause
+net use
+pause
+exit /b 0
