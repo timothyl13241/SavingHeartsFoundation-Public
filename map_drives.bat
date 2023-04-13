8d24263839005db976fc0a60a468a2bb86862f83
Timothy Le <14115751+timothyle97@users.noreply.github.com>
2023-04-13 12:16:36 -0700

    Update map_drives.bat
    - Prompt for username. This addresses using different user accounts.

--- map_drives.bat
+++ map_drives.bat
@@ -5,11 +5,12 @@ echo ########################   Script to Set Mapped Drives   ##################
 echo ########################     Revision 1b (04/13/2023)    #############################
 echo ######################################################################################
 echo:
-set /p "passw=Enter SHF_USER password: "
-cmdkey /add:172.30.16.234 /user:SHF\SHF_USER /pass:%passw%
+set /p "usr=Enter username (SHF\): "
+set /p "passw=Enter %usr% password: "
+cmdkey /add:172.30.16.234 /user:SHF\%usr% /pass:%passw%
 net use * /delete /y
-net use S: \\172.30.16.234\UCLA_SHF_Data %passw% /user:SHF\SHF_USER /p:yes
-net use U: \\172.30.16.234\UCLA_Deployment %passw% /user:SHF\SHF_USER /p:yes
+net use S: \\172.30.16.234\UCLA_SHF_Data %usr% /user:SHF\tle.adm /p:yes
+net use U: \\172.30.16.234\UCLA_Deployment %usr% /user:SHF\tle.adm /p:yes
 pause
 echo ######################################################################################
 echo Here are the currently mapped drives:
@@ -18,9 +19,9 @@ net use
 
 echo ######################################################################################
 echo Copying Cardea Preferences File
-echo F | xcopy "U:\Preference Files\20230411_user.config" %USERPROFILE%\AppData\Local\Cardiac_Insight,_Inc\Cardea_20.20_ECG.exe_Url_bnysl1bxpkuhajiypxmozcfbiagw0412\5.0.1.6\user.config /y /z
+echo D | xcopy "U:\Preference Files\20230411_user.config" "C:\Users\timot\AppData\Local\Cardiac_Insight,_Inc\Cardea_20.20_ECG.exe_Url_bnysl1bxpkuhajiypxmozcfbiagw0412\5.0.1.6\user.config" /y /z
 echo:
-echo If the above failed (i.e. 0 files copied), you need to import the preference file manually! 
-echo Otherwise, if you see 1 file, just check that the Data Acquisition location is set properly. 
+echo If the above failed, you need to import the preference file manually! 
+echo Otherwise, just check that the Data Acquisition location is set properly. 
 pause
 exit /b 0
