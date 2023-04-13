778d67f1b8f819e3cab9dafc3ec9ed204217e8a2
Timothy Le <14115751+timothyle97@users.noreply.github.com>
2023-04-13 05:02:27 -0700

    Update map_drives.bat
    - Added automatic copying of preferences file.

--- map_drives.bat
+++ map_drives.bat
@@ -1,17 +1,26 @@
 @echo off
 cls
 echo ######################################################################################
-echo ########################     Saving Hearts Foundation    #############################
 echo ########################   Script to Set Mapped Drives   #############################
 echo ########################     Revision 1a (04/11/2023)    #############################
 echo ######################################################################################
 echo:
-set /p "passw=Enter SHF_USER password: "
-cmdkey /add:172.30.16.234 /user:SHF\SHF_USER /pass:%passw%
+set /p "passw=Enter Admin password: "
+cmdkey /add:172.30.16.234 /user:SHF\tle.adm /pass:%passw%
 net use * /delete /y
-net use S: \\172.30.16.234\UCLA_SHF_Data %passw% /user:SHF\SHF_USER /p:yes
-net use U: \\172.30.16.234\UCLA_Deployment %passw% /user:SHF\SHF_USER /p:yes
+net use S: \\172.30.16.234\UCLA_SHF_Data %passw% /user:SHF\tle.adm /p:yes
+net use U: \\172.30.16.234\UCLA_Deployment %passw% /user:SHF\tle.adm /p:yes
 pause
+echo ######################################################################################
+echo Here are the currently mapped drives:
+echo:
 net use
+
+echo ######################################################################################
+echo Copying Cardea Preferences File
+echo D | xcopy "U:\Preference Files\20230411_user.config" "C:\Users\timot\AppData\Local\Cardiac_Insight,_Inc\Cardea_20.20_ECG.exe_Url_bnysl1bxpkuhajiypxmozcfbiagw0412\5.0.1.6\user.config" /y /z
+echo:
+echo If the above failed, you need to import the preference file manually! 
+echo Otherwise, just check that the Data Acquisition location is set properly. 
 pause
 exit /b 0
