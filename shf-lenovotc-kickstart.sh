59e4813a682eaef96757d13bcb90a9f1f91c7b24
Timothy Le <14115751+timothyle97@users.noreply.github.com>
2024-02-22 00:14:31 -0800

    Create shf-lenovotc-kickstart.sh

---
+++ shf-lenovotc-kickstart.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# File: shf-kickstart.sh
+#
+# Purpose: Kickstart SHF thin clients
+#
+# Author: Timothy Le
+#         <timothy.le@savingheartsfoundation.com>
+#
+
+echo "Installing updates..."
+apt-get -y update
+
+echo "Upgrading packages..."
+apt-get -y upgrade
+
+echo "Install management tools and necessary applications..."
+apt-get -y install ntpdate remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice
+
+echo "Initial time sync..."
+timedatectl set-timezone America/Los_Angeles
+ntpdate -b pool.ntp.org
+
+echo "Fix boot for Lenovo IdeaPad 100s..."
+apt-get -y install grub-efi-ia32-bin
+efibootmgr -o 0000,0001,2001,2002,2003
+update-grub
+grub-install /dev/mmcblk1
+
+echo "Disable sleep..."
+echo "AllowSuspend=no" >> /etc/systemd/sleep.conf
+echo "AllowHibernation=no" >> /etc/systemd/sleep.conf
+echo "AllowSuspendThenHibernate=no" >> /etc/systemd/sleep.conf
+echo "AllowHybridSleep=no" >> /etc/systemd/sleep.conf
+
+echo "Add WiFi network..."
+read -p "Enter AP1764 password: " pass
+nmcli device wifi connect "AP1764" password "$pass" iface wlan0
+
+echo "Configuration complete."
