d1f3af94dfa687e8f2c7b66cd06f35848faa415b
Timothy Le <14115751+timothyle97@users.noreply.github.com>
2024-02-22 02:26:45 -0800

    Update shf-lenovotc-kickstart.sh
    Added Remmina configuration file

--- shf-lenovotc-kickstart.sh
+++ shf-lenovotc-kickstart.sh
@@ -15,7 +15,9 @@ echo "Upgrading packages..."
 apt-get -y upgrade
 
 echo "Install management tools and necessary applications..."
-apt-get -y install ntpdate remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice
+apt-get -y install ntpdate remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice traceroute
+wget -O - "https://nitpro.servicedesk.atera.com/api/utils/AgentInstallScript/Linux/0013z00002fL1rhAAC?customerId=7" | bash
+curl https://raw.githubusercontent.com/virtualhere/script/main/install_server | sh
 
 echo "Initial time sync..."
 timedatectl set-timezone America/Los_Angeles
@@ -36,6 +38,25 @@ echo "AllowHybridSleep=no" >> /etc/systemd/sleep.conf
 echo "Add WiFi network..."
 read -p "Enter AP1764 password: " pass
 nmcli device wifi connect "AP1764" password "$pass"
+
+echo "Configure Remmina profile..."
+read -p "Enter terminal server IP: " rip
+mkdir /home/ubuntu/.remmina
+echo "[remmina]" > /home/ubuntu/.remmina/SHF.remmina
+echo "disableclipboard=1" >> /home/ubuntu/.remmina/SHF.remmina
+echo "ssh_auth=0" >> /home/ubuntu/.remmina/SHF.remmina
+echo "name=SHFTerminal" >> /home/ubuntu/.remmina/SHF.remmina
+echo "protocol=RDP" >> /home/ubuntu/.remmina/SHF.remmina
+echo "username=SHF_User" >> /home/ubuntu/.remmina/SHF.remmina
+echo "server=$rip" >> /home/ubuntu/.remmina/SHF.remmina
+echo "quality=2" >> /home/ubuntu/.remmina/SHF.remmina
+echo "colordepth=32" >> /home/ubuntu/.remmina/SHF.remmina
+echo "window_maximize=1" >> /home/ubuntu/.remmina/SHF.remmina
+echo "viewmode=4" >> /home/ubuntu/.remmina/SHF.remmina
+echo "password=" >> /home/ubuntu/.remmina/SHF.remmina
+echo "domain=SHF" >> /home/ubuntu/.remmina/SHF.remmina
+echo "scale=1" >> /home/ubuntu/.remmina/SHF.remmina
+echo "resolution_mode=1" >> /home/ubuntu/.remmina/SHF.remmina
 
 echo "Configuration complete."
 echo "Reboot? Press enter to drop to terminal [r]"
