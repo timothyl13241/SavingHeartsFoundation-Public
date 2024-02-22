e7ca9e4a4274f9131ec217fba7dbe39893a95d01
Timothy Le <14115751+timothyle97@users.noreply.github.com>
2024-02-22 02:39:15 -0800

    Create shf-remmina-start.sh

---
+++ shf-remmina-start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# File: shf-remmina-start.sh
+#
+# Purpose: Start configured Remmina profile at startup
+#
+# Author: Timothy Le
+#         <timothy.le@savingheartsfoundation.com>
+#
+
+file="/home/ubuntu.remmina/SHF.remmina"
+rip=$(cat "$file" | grep server | cut -d '=' -f2,4)
+
+while ! ping -c 4 $rip > /dev/null; 
+do 
+echo "The VM host is not available yet."
+sleep 1 
+done
+
+echo "VM host is now available. Starting Remmina"
+remmina -c /home/ubuntu/.remmina/SHF.remmina
