a152a0c846cc5b73e70e864d58b87dbb77bb3935
Timothy Le <14115751+timothyle97@users.noreply.github.com>
2024-02-22 10:56:14 -0800

    Update shf-remmina-start.sh
    Added remote IP to echo statements.

--- shf-remmina-start.sh
+++ shf-remmina-start.sh
@@ -13,9 +13,9 @@ rip=$(cat "$file" | grep server | cut -d '=' -f2,4)
 
 while ! ping -c 4 $rip > /dev/null; 
 do 
-echo "The VM host is not available yet."
+echo "The VM host at $rip is not available yet."
 sleep 1 
 done
 
-echo "VM host is now available. Starting Remmina"
+echo "VM host $rip is now available. Starting Remmina"
 remmina -c /home/ubuntu/.remmina/SHF.remmina
