56b8e4767646f6042f35e2f05741191f3b4aa88a
Timothy Le <14115751+timothyl13241@users.noreply.github.com>
2026-02-04 01:08:41 -0800

    Update MSI URL for Salt Minion installation

--- Salt/minion-install-windows.ps1
+++ Salt/minion-install-windows.ps1
@@ -1,5 +1,5 @@
 # Example Windows minion install via MSI
-$msiUrl = "https://repo.saltproject.io/salt/py3/win/3006/Salt-Minion-3006-Py3-AMD64.msi"
+$msiUrl = "https://packages.broadcom.com/artifactory/saltproject-generic/windows/3007.11/Salt-Minion-3007.11-Py3-AMD64.msi"
 $msiPath = "$env:TEMP\Salt-Minion.msi"
 Invoke-WebRequest -Uri $msiUrl -OutFile $msiPath
 Start-Process msiexec.exe -ArgumentList "/i `"$msiPath`" MASTER=salt.internal.savingheartsfoundation.com /qn" -Wait
